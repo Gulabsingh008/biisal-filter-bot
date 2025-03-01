c348c19e6caf27024c6f551a7da4fd0a4100b225
AF <gulabsinghrajput008@gmail.com>
2025-03-01 14:36:53 +0530

    Update insta.py

--- plugins/insta.py
+++ plugins/insta.py
@@ -2,13 +2,13 @@ from pyrogram import filters, Client
 import bs4, requests, re, asyncio
 import os, traceback, random
 from info import LOG_CHANNEL as DUMP_GROUP
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
     "Accept": "*/*",
     "Accept-Language": "en-US,en;q=0.5",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     "X-Requested-With": "XMLHttpRequest",
-    "Content-Length": "99",
     "Origin": "https://saveig.app",
     "Connection": "keep-alive",
     "Referer": "https://saveig.app/en",
@@ -17,50 +17,34 @@ headers = {
 @Client.on_message(filters.regex(r'https?://.*instagram[^\s]+') & filters.private)
 async def link_handler(Mbot, message):
     link = message.matches[0].group(0)
-    global headers
     try:
-        m = await message.reply_sticker("CAACAgUAAxkBAAJwgmYsfgvGbfH7xYqlNzyFsMSOpPdXAAIGBwACc7LBVBHH8bMK6dZAHgQ")
-        url = link.replace("instagram.com", "ddinstagram.com")
-        url = url.replace("==", "%3D%3D")
-        
-        if '/reel/' in url:  # Handling Reels
-            ddinsta = True 
-            getdata = requests.get(url).text
-            soup = bs4.BeautifulSoup(getdata, 'html.parser')
-            meta_tag = soup.find('meta', attrs={'property': 'og:video'})
-            
-            try:
-                content_value = f"https://ddinstagram.com{meta_tag['content']}"
-            except:
-                pass 
-            if not meta_tag:
-                ddinsta = False
-                meta_tag = requests.post("https://saveig.app/api/ajaxSearch", data={"q": link, "t": "media", "lang": "en"}, headers=headers)
-                
-                if meta_tag.ok:
-                    res = meta_tag.json()
-                    meta = re.findall(r'href="(https?://[^"]+)"', res['data'])
-                    content_value = meta[0]
-                else:
-                    return await message.reply("Oops something went wrong")
-            
+        m = await message.reply_sticker("CAACAgUAAxkBAAKD6mcTLETIv8Yrc83GnIcYXP9Ip6LHAAKgCQACJ_YYVbZRplnJDKrJNgQ")
+
+        if '/reel/' in link:  # Handling Reels
+            meta_tag = requests.post("https://saveig.app/api/ajaxSearch", data={"q": link, "t": "media", "lang": "en"}, headers=headers)
+            if meta_tag.ok:
+                res = meta_tag.json()
+                meta = re.findall(r'href="(https?://[^"]+)"', res['data'])
+                content_value = meta[0]
+            else:
+                return await message.reply("Oops something went wrong")
+
             try:
                 dump_file = await message.reply_video(content_value, caption="Download By 👉 @af")
-            except:
+            except Exception as e:
                 downfile = f"{os.getcwd()}/{random.randint(1, 10000000)}"
                 with open(downfile, 'wb') as x:
-                    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
                     x.write(requests.get(content_value, headers=headers).content)
                 dump_file = await message.reply_video(downfile, caption="Download By 👉 @af")
-        
-        elif '/p/' in url:  # Handling Posts
+
+        elif '/p/' in link:  # Handling Posts
             meta_tag = requests.post("https://saveig.app/api/ajaxSearch", data={"q": link, "t": "media", "lang": "en"}, headers=headers)
             if meta_tag.ok:
                 res = meta_tag.json()
                 meta = re.findall(r'href="(https?://[^"]+)"', res['data'])
             else:
                 return await message.reply("Oops something went wrong")
-            
+
             for i in range(len(meta) - 1):
                 com = await message.reply_text(meta[i])
                 await asyncio.sleep(1)
@@ -68,16 +52,16 @@ async def link_handler(Mbot, message):
                     dump_file = await message.reply_video(com.text, caption="Download By 👉 @af")
                     await com.delete()
                 except:
-                    pass 
-        
-        elif "stories" in url:  # Handling Stories
+                    pass
+
+        elif "stories" in link:  # Handling Stories
             meta_tag = requests.post("https://saveig.app/api/ajaxSearch", data={"q": link, "t": "media", "lang": "en"}, headers=headers)
             if meta_tag.ok:
                 res = meta_tag.json()
                 meta = re.findall(r'href="(https?://[^"]+)"', res['data'])
             else:
                 return await message.reply("Oops something went wrong")
-            
+
             try:
                 dump_file = await message.reply_video(meta[0], caption="Download By 👉 @af")
             except:
@@ -91,9 +75,9 @@ async def link_handler(Mbot, message):
 
         if 'dump_file' in locals():
             await dump_file.forward(DUMP_GROUP)
-        
+
         await m.delete()
-    
+
     except Exception as e:
         try:
             await message.reply(f"400: Sorry, Unable To Find It Make Sure It's Publicly Available :)")
@@ -102,7 +86,7 @@ async def link_handler(Mbot, message):
                 await Mbot.send_message(DUMP_GROUP, f"Instagram Error: {e} {link}")
                 await Mbot.send_message(DUMP_GROUP, traceback.format_exc())
             await message.reply(f"400: Sorry, Unable To Find It. Try another or report it to @af")
-    
+
     finally:
         if 'dump_file' in locals():
             if DUMP_GROUP:
@@ -111,4 +95,3 @@ async def link_handler(Mbot, message):
         if 'downfile' in locals():
             os.remove(downfile)
         await message.reply("<a href='https://t.me/af'>af</a>")
-                       
